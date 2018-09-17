823fbaf4c0742837ab3cdc4454408027a4d811e2
Chandresh <chandresh989@gmail.com>
2018-09-17 10:52:30 +0530

    Initial Commit

---
+++ MacroFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MacroFit
+//
+//  Created by Chandresh Singh on 17/09/18.
+//  Copyright © 2018 Chandresh Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
