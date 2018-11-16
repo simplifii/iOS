a77b974d85a820da774969f06d56d078596548ab
Chandresh <chandresh@simplifii.com>
2018-11-16 11:32:07 +0530

    fetch contacts from phonebook commented code

--- MacroFit/ViewControllers/Fitness/FitnessViewController.swift
+++ MacroFit/ViewControllers/Fitness/FitnessViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+//import ContactsUI
 
 class FitnessViewController: BaseViewController {
     @IBOutlet weak var navbarView: UIView!
     
+//    let contactStore = CNContactStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        addMenuNavbarInView(navbarView: navbarView)
     }
+    
+    
+//    func getPhoneBookContacts() {
+//        var contacts = [CNContact]()
+//        let keys = [CNContactGivenNameKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey]
+//        let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+//
+//
+//        self.contactStore.requestAccess(for: .contacts, completionHandler: {(granted,error) in
+//            if (error != nil) {
+//                print("access denied")
+//            }
+//            if granted {
+//                do {
+//                    try self.contactStore.enumerateContacts(with: request) {
+//                        (contact, stop) in
+//                        // Array containing all unified contacts from everywhere
+//                        print(contact.phoneNumbers[0].value.stringValue)
+//                        print(contact.emailAddresses[0].value)
+//
+//                        contacts.append(contact)
+//                    }
+//                }
+//                catch {
+//                    print("unable to fetch contacts")
+//                }
+//            }
+//        })
+//    }
 
 }
