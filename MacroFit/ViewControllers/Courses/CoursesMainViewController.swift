90640117370919bc941c68ae3fe94ba32f46e23f
Jon Pierce-Ruhland <jpierceruhland@theblaze.com>
2018-11-20 00:04:56 -0600

    Load course data, display categories

--- MacroFit/ViewControllers/Courses/CoursesMainViewController.swift
+++ MacroFit/ViewControllers/Courses/CoursesMainViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 import SwiftyJSON
 
+class CoursesContainer: BaseViewController {
+    @IBAction func backPressed(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
 class CoursesMainViewController: BaseViewController {
     
     fileprivate var coursesJSON:JSON = []
+    fileprivate let reuseIdentifier = "courseCell"
+    fileprivate var courseCategories:[String:[JSON]] = [:]
+    fileprivate var categoryImages:[String:String] = [:]
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -20,10 +29,33 @@ class CoursesMainViewController: BaseViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.register(UINib(nibName: "CourseTableViewCell", bundle: nil), forCellReuseIdentifier: reuseIdentifier)
+        
         APIService.getCourses(completion: { [weak self] success, msg, data in
             if success {
                 self?.coursesJSON = data
-                self?.tableView.reloadData()
+                var tempCourseCategories:[String:[JSON]] = [:]
+                var tempCategoryImages:[String:String] = [:]
+                for course in data {
+                    if let courseTags = course.1["tags"].rawString() {
+                        for t in courseTags.split(separator: ",") {
+                            let key = String(t)
+                            if tempCourseCategories[key] == nil {
+                                tempCourseCategories[key] = [JSON]()
+                                tempCategoryImages[key] = course.1["image"].rawString()
+                            }
+                            
+                            tempCourseCategories[key]?.append(course.1)
+                        }
+                    }
+                }
+                
+                self?.courseCategories = tempCourseCategories
+                self?.categoryImages = tempCategoryImages
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
             } else {
                 self?.showAlertMessage(title: msg, message: nil)
             }
@@ -45,10 +77,27 @@ class CoursesMainViewController: BaseViewController {
 
 extension CoursesMainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return courseCategories.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let key = courseCategories.keys.sorted()[indexPath.row]
+//        let imageURL = categoryImages[key]
+        
+        if let courseCell = cell as? CourseTableViewCell {
+            courseCell.titleLabel.text = key
+            
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Init a model with all of the courses
     }
 }
